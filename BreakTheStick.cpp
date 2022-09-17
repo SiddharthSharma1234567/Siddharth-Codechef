cf2ed9b577ef4fb9d70f928ce389585b80b77f80
SiddharthSharma1234567 <112341654+SiddharthSharma1234567@users.noreply.github.com>
2022-09-17 23:06:29 +0530

    Add files via upload

---
+++ BreakTheStick.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,a;
+	    cin>>n>>x;
+	    a=n-x;
+	    if(x%2!=0){
+	        cout<<"YES\n";
+	    }
+	    else{
+	        if(n%2==0 && x%2==0){
+	            cout<<"Yes\n";
+	        }
+	        else{
+	            cout<<"NO\n";
+	        }
+	    }
+	}
+	return 0;
+}
